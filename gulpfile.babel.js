48b80d381d33df1a78b4e075ae1869eb57a02b63
Jae Hoon Yang <didwogns789@naver.com>
2020-08-01 23:17:57 +0900

    change build -> dist

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ import gImage from "gulp-image";
 import sass from "gulp-sass";
 sass.compiler = require("node-sass");
 import autoprefixer from "gulp-autoprefixer";
-import csso from "gulp-csso";
+import minify from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
 import ghPages from "gulp-gh-pages";
@@ -17,28 +17,28 @@ const routes = {
   pug: {
     watch: "src/**/*.pug",
     src: "src/*.pug",
-    dest: "build",
+    dest: "dist",
   },
   scss: {
     watch: "src/scss/**/*.scss",
     src: "src/scss/style.scss",
-    dest: "build/css",
+    dest: "dist/css",
   },
   image: {
     src: "src/img/*",
-    dest: "build/img",
+    dest: "dist/img",
   },
   js: {
     watch: "src/js/**/*.js",
     src: "src/js/*.js",
-    dest: "build/js",
+    dest: "dist/js",
   },
 };
 
-const clean = () => del(["build", ".publish"]);
+const clean = () => del(["dist", ".publish"]);
 
 const webserver = () =>
-  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+  gulp.src("dist").pipe(ws({ livereload: true, open: true }));
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
@@ -54,8 +54,14 @@ const scss = () =>
   gulp
     .src(routes.scss.src)
     .pipe(sass.sync().on("error", sass.logError))
-    .pipe(autoprefixer({ overrideBrowserslist: "last 2 versions" }))
-    .pipe(csso())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: "last 2 versions",
+        flexbox: true,
+        grid: "autoplace",
+      })
+    )
+    .pipe(minify())
     .pipe(gulp.dest(routes.scss.dest));
 
 const js = () =>
@@ -69,12 +75,12 @@ const js = () =>
         ],
       })
     )
-    .pipe(gulp.dest("build/js"));
+    .pipe(gulp.dest("dist/js"));
 
 const image = () =>
   gulp.src(routes.image.src).pipe(gImage()).pipe(gulp.dest(routes.image.dest));
 
-const gh = () => gulp.src("build/**/*").pipe(ghPages());
+const gh = () => gulp.src("dist/**/*").pipe(ghPages());
 
 const prepare = gulp.series([clean]);
 
@@ -82,8 +88,8 @@ const assets = gulp.series([pug, image, scss, js]);
 
 const live = gulp.parallel([webserver, watch]);
 
-export const build = gulp.series([prepare, assets]);
+export const dist = gulp.series([prepare, assets]);
 
-export const dev = gulp.series([build, live]);
+export const dev = gulp.series([dist, live]);
 
-export const deploy = gulp.series([build, gh, clean]);
+export const deploy = gulp.series([dist, gh, clean]);
